cc4c50ffe10c69980ca819bcf3fa71d5ec7c500c
tetedange13 <felix.deslacs@gmail.com>
2025-09-17 17:06:22 +0200

    Improved script

--- tests/panelCapture/run_happy_SAREK.sh
+++ tests/panelCapture/run_happy_SAREK.sh
@@ -5,8 +5,14 @@ source /etc/profile.d/conda.sh && conda activate /home/olivier/.conda/envs/hap.p
 
 set -euo pipefail
 
+# MAIN:
+if [ $# -lt 3 ]; then
+    echo "ERROR: Script takes 3 mandatory args"
+	echo "USAGE: bash run_happy_SAREK.sh /path/to/runID/MobiCorail HG002 Twist-exome.bed"
+	echo ""
+    exit 1
+fi
 
-# Common variables:
 prfx_tested=$1
 SAMPLE=$2
 usedBed=$3
@@ -21,22 +27,24 @@ set -x  # Activation of env is too verbose set 'DEBUG' from here
 # Running hap.py takes ~ 15 min by sample
 
 # 'DeepVariant' VCF:
+# MEMO: DV vcf in Sarek is not 'refCall' filtered -> used '.norm.' one
+#       They sould be kept by 'normAndMerge.wdl' (by default)
 happy_exomeTwist \
     "$SAMPLE" \
-    $(find "$prfx_tested"/variant_calling/deepvariant/"$SAMPLE"* -type f -name "${SAMPLE}*.deepvariant.vcf.gz") \
+    $(find "$prfx_tested"/variant_calling/deepvariant/* -type f -name "${SAMPLE}*.deepvariant.norm.vcf.gz") \
     "$usedBed" &
 
 # 'HaplotypeCaller' VCF:
 # MEMO: We use 'filtered' one bellow
 happy_exomeTwist \
     "$SAMPLE" \
-    $(find "$prfx_tested"/variant_calling/haplotypecaller/"$SAMPLE"* -type f -name "${SAMPLE}*.filtered.vcf.gz") \
+    $(find "$prfx_tested"/variant_calling/haplotypecaller/* -type f -name "${SAMPLE}*.haplotypecaller.norm.vcf.gz") \
     "$usedBed" &
 
 # 'merged' VCF:
 happy_exomeTwist \
    "$SAMPLE" \
-   $(find "$prfx_tested"/variant_calling/merge/"$SAMPLE"* -type f -name "${SAMPLE}*.vcf.gz") \
+   $(find "$prfx_tested"/variant_calling/merge/* -type f -name "${SAMPLE}*.vcf.gz") \
    "$usedBed" &
 
 
